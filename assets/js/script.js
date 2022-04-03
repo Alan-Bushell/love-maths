cc63b8729a8cec4cc7e5aeeaefa95fb661fd3b74
Alan-Bushell <alanbushell91@gmail.com>
2022-04-03 07:03:50 +0000

    added subtract function

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function(){
             checkAnswer();
         }
     })
-    
+
     runGame("addition");
 })
 
@@ -41,6 +41,8 @@ function runGame(gameType){
         displayMultiplyQuestion(num1, num2);
     } else if(gameType === "subtract"){
         displaySubtractQuestion(num1, num2);
+    } else if(gameType === "division"){
+        displayDivisionQuestion(num1, num2);
     }
       else{
         alert(`Unknown game type: ${gameType}`);
@@ -83,7 +85,11 @@ function calculateCorrectAnswer(){
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else {
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    }
+    
+      else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -122,5 +128,16 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+}
 
+function displayDivisionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    if (operand1 % operand2 > 1){
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    } else{
+        return
+    }
+    document.getElementById("operator").textContent = "/";
 }
